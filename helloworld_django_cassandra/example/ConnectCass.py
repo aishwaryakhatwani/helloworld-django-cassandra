001220c92ef6a81649288bd352dcc7caeb6875ff
aishwaryakhatwani <ak3518@rit.edu>
2021-07-03 12:25:12 -0400

    Update ConnectCass.py

--- helloworld_django_cassandra/example/ConnectCass.py
+++ helloworld_django_cassandra/example/ConnectCass.py
@@ -26,6 +26,6 @@ class Connect:
         query = "select temperature as temp from raw_weather_data where year = "+ year+ " and month = " + month + " and wsid = '" + airbase + "' ALLOW FILTERING;"
         print(query)
         statement = SimpleStatement(query)
-        data = self.session.execute(statement).current_rows
-        print(data.current_rows)
+        data = self.session.execute(statement).all()
+        print(data)
         return data
