6d5b2b1c1dc23b731adb857c845943a0d1f1e544
aishwaryakhatwani <ak3518@rit.edu>
2021-07-03 12:18:52 -0400

    Update ConnectCass.py

--- helloworld_django_cassandra/example/ConnectCass.py
+++ helloworld_django_cassandra/example/ConnectCass.py
@@ -27,5 +27,5 @@ class Connect:
         print(query)
         statement = SimpleStatement(query)
         data = self.session.execute(statement)
-        print(data)
+        print(data.temp)
         return data
