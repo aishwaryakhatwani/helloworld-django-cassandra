58b65230311bb52910ff33d004fd5f69a2634fc0
aishwaryakhatwani <ak3518@rit.edu>
2021-07-13 22:46:50 -0400

    Update settings.py

--- Weather_data/helloworld_django_cassandra/settings.py
+++ Weather_data/helloworld_django_cassandra/settings.py
@@ -134,4 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join('Weather_data','static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+                os.path.join('Weather_data','staticfiles'), # if your static files folder is named "staticfiles"
+)
