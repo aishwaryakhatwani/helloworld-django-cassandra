9a7a501e399e6aca6824ab23f9a44fb45dcc8635
aishwaryakhatwani <ak3518@rit.edu>
2021-07-15 19:19:54 -0400

    Update urls.py

--- Weather_data/helloworld_django_cassandra/urls.py
+++ Weather_data/helloworld_django_cassandra/urls.py
@@ -16,6 +16,8 @@ Includi ng another URLconf
 from django.conf.urls import url
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from config import views
 
